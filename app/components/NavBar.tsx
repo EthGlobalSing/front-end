ad9a27346b032579f52ca4b5cdb9c7caef82b83e
mathis sergent <sergent.mathis@gmail.com>
2024-09-21 14:48:09 +0800

    Explore apps and revoke access

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Link, Navbar, NavbarContent, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from "@nextui-org/react";
+import { Link, Navbar, NavbarContent, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, NavbarBrand, Button } from "@nextui-org/react";
 import { dmSerifText } from "../shared/fonts";
 
 interface NavBarProps {
@@ -8,35 +8,40 @@ interface NavBarProps {
 
 export function NavBar(props: NavBarProps) {
     const menuItems = [
+        "Explore apps",
         "Assets",
         "History",
         "Settings",
-      ];
+    ];
 
     return (<Navbar onMenuOpenChange={props.setIsMenuOpen} className={props.isMenuOpen ? 'bg-lightGreen' : 'bg-darkGreen'}>
-    <NavbarContent>
-      <NavbarMenuToggle
-        aria-label={props.isMenuOpen ? "Close menu" : "Open menu"}
-        className={`sm:hidden ${props.isMenuOpen ? 'text-darkGreen' : 'text-white'}`}
-      />
-      {/* <NavbarBrand>
-        <p className="font-bold text-inherit">MiniSafe</p>
-      </NavbarBrand> */}
-    </NavbarContent>
+        <NavbarContent>
+            <NavbarMenuToggle
+                aria-label={props.isMenuOpen ? "Close menu" : "Open menu"}
+                className={`sm:hidden ${props.isMenuOpen ? 'text-darkGreen' : 'text-white'}`}
+            />
+            <NavbarBrand>
+                <p className="font-bold text-inherit text-lightGreen">MiniSafe</p>
+            </NavbarBrand>
+        </NavbarContent>
 
-    <NavbarMenu className='bg-lightGreen'>
-      {menuItems.map((item, index) => (
-        <NavbarMenuItem key={`${item}-${index}`}>
-          <Link
-            color={"foreground"}
-            className={`w-full text-2xl mb-8 text-darkGreen ${dmSerifText.className}`}
-            href="#"
-            size="lg"
-          >
-            {item}
-          </Link>
-        </NavbarMenuItem>
-      ))}
-    </NavbarMenu>
-  </Navbar>);
+        <NavbarMenu className='bg-lightGreen flex justify-between'>
+            <div>
+                {menuItems.map((item, index) => (
+                    <NavbarMenuItem key={`${item}-${index}`}>
+                        <Link
+                            color={"foreground"}
+                            className={`w-full text-2xl mb-8 text-darkGreen ${dmSerifText.className} ${index === 0 ? 'mt-12 mb-20' : ''}`}
+                            href="#"
+                            size="lg"
+                        >
+                            {item}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </div>
+
+            <Button className="mb-8 bg-darkRed text-lightGreen" disabled>Revoke access</Button>
+        </NavbarMenu>
+    </Navbar>);
 }
--- app/showAssets/page.tsx
+++ app/showAssets/page.tsx
@@ -5,9 +5,11 @@ import { useEffect, useState } from 'react';
 import { CircularProgress } from "@nextui-org/react";
 import Image from "next/image";
 import { dmSerifText } from "../shared/fonts";
+import { NavBar } from "../components/NavBar";
 
 export default function ShowAssets() {
-    const [loading, setLoading] = useState(true); // Initially set to true for loading simulation
+    const [loading, setLoading] = useState(false); // Initially set to true for loading simulation
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const [safeWallets, setSafeWallets] = useState<Safe[]>([{
         chainId: 1,
@@ -32,22 +34,25 @@ export default function ShowAssets() {
             {loading ? (
                 <CircularProgress className='m-auto' color="default" aria-label="Loading..." />
             ) : (
-                <div className='p-10 min-h-screen flex flex-col gap-12 items-center'>
-                    <div className="flex flex-col gap-6">
-                        <h1 className={`${dmSerifText.className} text-lightGreen text-3xl`}>$839.29</h1>
-                    </div>
-                    <div className="w-full">
-                        {safeWallets.map((safe, index) => (
-                            <div key={index} className="w-full p-8 bg-lightGreen rounded-xl flex gap-4 items-center justify-between">
-                                <div className="flex gap-4 items-center">
-                                    <Image src={'/chains/ethereum.svg'} alt="Ethereum" width={20} height={20} />
-                                    <p className="text-xl text-darkGreen">Ethereum</p>
+                <>
+                    <NavBar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+                    <div className='p-10 min-h-screen flex flex-col gap-12 items-center'>
+                        <div className="flex flex-col gap-6">
+                            <h1 className={`${dmSerifText.className} text-lightGreen text-3xl`}>$839.29</h1>
+                        </div>
+                        <div className="w-full">
+                            {safeWallets.map((safe, index) => (
+                                <div key={index} className="w-full p-6 bg-lightGreen rounded-xl flex gap-4 items-center justify-between">
+                                    <div className="flex gap-4 items-center">
+                                        <Image src={'/chains/ethereum.svg'} alt="Ethereum" width={20} height={20} />
+                                        <p className="text-xl text-darkGreen">Ethereum</p>
+                                    </div>
+                                    <p className={`text-lg text-darkGreen ${dmSerifText.className}`}>$38.38</p>
                                 </div>
-                                <p className={`text-lg text-darkGreen ${dmSerifText.className}`}>$38.38</p>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
-                </div>
+                </>
             )}
         </main>
     );
