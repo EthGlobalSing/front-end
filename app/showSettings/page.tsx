8a33e5b4fda367e7e1b3d41d499d5a2f1faa8d04
mathis sergent <sergent.mathis@gmail.com>
2024-09-21 15:12:17 +0800

    Settings

--- app/showSettings/page.tsx
+++ app/showSettings/page.tsx
@@ -1,5 +1,35 @@
-import { Button } from "@nextui-org/react";
+'use client';
+
+import { Safe } from "../types/safe";
+import { useEffect, useState } from 'react';
+import { Button, CircularProgress } from "@nextui-org/react";
+import Image from "next/image";
+import { dmSerifText } from "../shared/fonts";
+import { NavBar } from "../components/NavBar";
 
 export default function ShowSettings() {
-    return (<Button className="mb-8 bg-darkRed text-lightGreen" disabled>Revoke access</Button>);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const [transactions, setTransactions] = useState([{
+        amountInUSD: 938.92,
+        direction: 'out',
+        chainId: 1,
+        to: {
+            username: 'pybast',
+            address: "0xZJdhb28dJ"
+        }
+    }]);
+
+    return (
+        <main className="bg-darkGreen">
+
+            <>
+                <NavBar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} windowName="Settings" />
+                <div className='p-10 min-h-screen flex flex-col gap-12 items-center'>
+                    <Button className="mb-8 bg-darkRed text-lightGreen" disabled>Revoke all access</Button>
+                </div>
+            </>
+
+        </main>
+    );
 }
