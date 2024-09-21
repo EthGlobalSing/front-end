e16a2bc59250b3758560eda2a7c2e5de067cb163
mathis sergent <sergent.mathis@gmail.com>
2024-09-21 13:46:47 +0800

    Setup

--- app/setup/page.tsx
+++ app/setup/page.tsx
@@ -1,7 +1,18 @@
+'use client';
+
+import { Safe } from "../types/safe";
 import { ErrorDisplay } from "./components/ErrorDisplay";
+import { useEffect, useState } from 'react';
+import { SafeDetectedDisplay } from "./components/SafeDetectedDisplay";
 
 export default function Setup() {
+    const [safeWallets, setSafeWallets] = useState<Safe[]>([{
+        chainId: 1,
+        amountInUSD: 938,
+        thresholds: 3,
+        addresses: ["0xZJdhb28dJ", "0xZJdhb28dJ"]
+    }]);
 
     return (<main className={`p-10 bg-darkGreen flex justify-center`}>
-        <ErrorDisplay /></main>);
+        {safeWallets ? <SafeDetectedDisplay safeWallets={safeWallets} /> : <ErrorDisplay />}</main>);
 }
