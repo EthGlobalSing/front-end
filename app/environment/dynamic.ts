82b2db4bb8a07f8e4064c609a2f33f9ce5e94d61
mathis sergent <sergent.mathis@gmail.com>
2024-09-21 18:23:19 +0800

    fix for deployment

--- app/environment/dynamic.ts
+++ app/environment/dynamic.ts
@@ -1,3 +1,3 @@
-// if(process.env.NEXT_PUBLIC_DYNAMIC_ENV === undefined) throw Error('DYNAMIC_ENV is undefined');
+if (process.env.NEXT_PUBLIC_DYNAMIC_ENV === undefined) throw Error('DYNAMIC_ENV is undefined');
 
-// export const dynamic_env = process.env.NEXT_PUBLIC_DYNAMIC_ENV;
+export const dynamic_env = process.env.NEXT_PUBLIC_DYNAMIC_ENV;
